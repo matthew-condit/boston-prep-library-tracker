54bb7abcb6cdd363c56b7c26b3438e66960bb464
Matthew Condit <matt.condit@outlook.com>
2017-12-28 14:46:36 -0500

    hooking more up

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,17 +1,48 @@
 import * as React from 'react';
 import { compose, withState, withHandlers } from 'recompose';
+import {Redirect} from 'react-router-dom';
 import axios from 'axios';
-import classnames from 'classnames';
+import actions from '../../redux/actions/auth';
 import LabeledInput from '../common/labeledInput/labeledInput';
+
 import './login.css';
 
-
-const hasErrors = () => {
-
+const mapStateToProps = state => {
+    return {
+        authenticated: state.authReducer.authenticated
+    }
 }
 
-const onLoginButtonClicked = props => async () => {
-    
+const mapDispatchToProps = dispatch => {
+    return {
+        login: () => {
+            dispatch(actions.login())
+        }
+    }
+}
+
+const onLoginButtonClicked = ({ email, password, setErrors, login }) => async () => {
+    if (!email || !password) {
+        setErrors({
+            email: !email,
+            password: !password
+        });
+        return;
+    }
+    try {
+        const res = await axios.post('/users/login', {
+            email,
+            password
+        });
+        if (res.status === 200) {
+            login()
+        } else {
+            console.log(res.data.message)
+        }
+    } catch (e) {
+
+    }
+
 
 }
 
@@ -26,12 +57,26 @@ const enhance = compose(
     })
 );
 
-const loginPure = ({email, onEmailChange,
-                    password, onPasswordChange,
-                    onLoginButtonClicked
-                    }: any) => {
+const loginPure = ({ email, onEmailChange,
+    password, onPasswordChange,
+    onLoginButtonClicked,
+    errors,
+    authenticated}: any) => {
+    if (authenticated) {
+        return (
+            <Redirect to='/users'/>
+        )
+    }
     return (
         <div>
+            <div>
+                {errors.email &&
+                    <div className='login-error-message'>Please provide your email</div>
+                }
+                {errors.password &&
+                    <div className='login-error-message'>Please provide your password</div>
+                }
+            </div>
             <LabeledInput label='Email' value={email} onChange={onEmailChange} />
             <LabeledInput label='Password' value={password} onChange={onPasswordChange} />
             <button onClick={onLoginButtonClicked} className='login-page__submit-button'>Log In</button>
